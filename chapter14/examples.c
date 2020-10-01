384e2241ecaf81f2c82a9b06ea4977e7aac6feef
spainisnotequal <spainisnotequal@gmail.com>
2020-10-01 08:46:38 +0200

    Explicit cast after using malloc

--- chapter14/examples.c
+++ chapter14/examples.c
@@ -4,12 +4,13 @@
 int main(int argc, char *argv[]) {
 
         // Array of 10 integers allocated dynamically
-        int *x = malloc(10 * sizeof(int));
+        int *x = (int *) malloc(10 * sizeof(int));
         printf("sizeof(x) = %zu (size of a pointer to an int)\n", sizeof(x));
 
         // Array of 10 integers allocated statically
         int y[10];
         printf("sizeof(y) = %zu (size of an array of 10 ints)\n", sizeof(y));
 
+        
         return 0;
 }
